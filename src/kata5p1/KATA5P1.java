efd1615da8048a9cd7eb8ea797f5beb07166b618
DaniCerv <DaniCerv@192.168.1.109>
2017-12-29 00:43:12 +0100

    Kata5.Versión 4.Se intenta realizar una conexión a la base de datos de una máquina virtual y se ejecutan una serie de consultas aunque algunas están expuestas como comentarios.

--- src/kata5p1/KATA5P1.java
+++ src/kata5p1/KATA5P1.java
@@ -1,9 +1,7 @@
 package kata5p1;
 
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -13,44 +11,35 @@ import java.sql.Statement;
 
 public class KATA5P1 {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException{
-        Class.forName("org.sqlite.JDBC");
-        Connection connection = DriverManager.getConnection("jdbc:sqlite:"
-        + "C:\\Users\\DaniCerv\\Downloads\\SQLiteDatabaseBrowserPortable\\Data\\KATA5.db");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException{
+        Class.forName("");
+        Connection connection = DriverManager.getConnection(
+                "http://127.0.0.1:5560/isqlplus/workspace.uix","system","orcl");
         Statement statement = (Statement) connection.createStatement();
-        String query="SELECT * FROM PEOPLE;";
+        String query="SELECT * FROM HISTORICO_CAMBIOS;";
         ResultSet rs = statement.executeQuery(query);
         while(rs.next()){
              System.out.println("ID= " + rs.getInt("ID"));
              System.out.println("NAME= " + rs.getString("NAME"));
         }
-        query = "CREATE TABLE IF NOT EXISTS MAIL "
-        + "('Id' INTEGER PRIMARY KEY AUTOINCREMENT , 'Mail' TEXT NOT NULL);";
-        statement.execute(query);
         
-        FileReader fl=null;
-        BufferedReader bf=null;
-        String fileName ="C:\\Users\\DaniCerv\\Desktop"
-         + "\\Ficheros para la práctica en laboratorio 5 -20171227\\emails.txt";
-        
-        try{
-            fl=new FileReader(fileName);
-            bf= new BufferedReader(fl);
-            String mail;
-            while((mail=bf.readLine())!=null){
-                if(!mail.contains("@")){
-                    continue;
-                }
-                query =	"INSERT	INTO	MAIL	(Mail)	VALUES	('"+ mail +"');”";
-                statement.execute(query);
-            }
-        }catch(IOException e){
-                    e.printStackTrace();
-        }
-        
-        query = "SELECT COUNT(*) FROM MAIL";
-        rs=statement.executeQuery(query);
-        System.out.println("Número de registros de la tabla MAIL: " + rs.getInt(1));
+        /**
+         * Consultas a realizar:
+         * Contar el número de registros de la tabla.
+         * SELECT COUNT (*) FROM HISTORICO_CAMBIOS;
+         * Contar el número de registros de una determinada DIVISA_DESDE y
+         * DIVISA_A
+         * SELECT COUNT(DIVISA_DESDE, DIVISA_A) FROM HISTORICO_CAMBIOS;
+         * 
+         * Listar todos	los cambios que	tenemos	desde una DIVISA_DESDE a 
+         * una DIVISA_A.
+         * Para ello se podría crear una auditoría para guardar los cambios
+         * en la tabla a través de un trigger genérico.
+         * 
+         * Si fuese necesario se podría realiza alguna query que pudiese ser
+         * necesaria para MoneyCalculator.
+         * 
+         */
         
     }    
 }
