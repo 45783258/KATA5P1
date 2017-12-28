9b459f3078d19583d9fa02934c65f39c02edfd71
DaniCerv <DaniCerv@192.168.1.109>
2017-12-28 03:21:15 +0100

    Kata5.Versión 3.Se realiza la tercera conexión a la base de datos KATA5 y se ejecuta una serie de consultas que lee el fichero de emails que se ha utilizado anteriormente y se añaden dichos emails como registros de la base de datos.

--- src/kata5p1/KATA5P1.java
+++ src/kata5p1/KATA5P1.java
@@ -1,6 +1,10 @@
 package kata5p1;
 
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -9,7 +13,7 @@ import java.sql.Statement;
 
 public class KATA5P1 {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException{
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException{
         Class.forName("org.sqlite.JDBC");
         Connection connection = DriverManager.getConnection("jdbc:sqlite:"
         + "C:\\Users\\DaniCerv\\Downloads\\SQLiteDatabaseBrowserPortable\\Data\\KATA5.db");
@@ -23,6 +27,30 @@ public class KATA5P1 {
         query = "CREATE TABLE IF NOT EXISTS MAIL "
         + "('Id' INTEGER PRIMARY KEY AUTOINCREMENT , 'Mail' TEXT NOT NULL);";
         statement.execute(query);
+        
+        FileReader fl=null;
+        BufferedReader bf=null;
+        String fileName ="C:\\Users\\DaniCerv\\Desktop"
+         + "\\Ficheros para la práctica en laboratorio 5 -20171227\\emails.txt";
+        
+        try{
+            fl=new FileReader(fileName);
+            bf= new BufferedReader(fl);
+            String mail;
+            while((mail=bf.readLine())!=null){
+                if(!mail.contains("@")){
+                    continue;
+                }
+                query =	"INSERT	INTO	MAIL	(Mail)	VALUES	('"+ mail +"');”";
+                statement.execute(query);
+            }
+        }catch(IOException e){
+                    e.printStackTrace();
+        }
+        
+        query = "SELECT COUNT(*) FROM MAIL";
+        rs=statement.executeQuery(query);
+        System.out.println("Número de registros de la tabla MAIL: " + rs.getInt(1));
         
     }    
 }
